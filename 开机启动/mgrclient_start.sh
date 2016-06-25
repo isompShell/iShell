a07f16a6a0f4cabda7c3236b2595984bbcb5ea09
hubin <alger_bin@foxmail.com>
2016-06-26 07:01:54 +0800

    update

--- 开机启动/mgrclient_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if ! grep -i "mgrclient.sh" /usr/local/bin/start.sh;then
-	echo -e "/usr/local/client/mgrclient.sh" >>/usr/local/bin/start.sh
-fi
-java -jar /usr/local/client/mgr_client.jar >/dev/dull &
