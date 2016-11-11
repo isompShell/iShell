85359e19efd0ee06b5b49540426e2c97ce557a6b
root <root@DESKTOP-DVPPGP1.localdomain>
2016-11-11 12:38:20 +0800

    交换机改密

--- 改密与账号收集/shell_create.sh
+++ 改密与账号收集/shell_create.sh
@@ -6,13 +6,25 @@
 #Date:2016-11-09
 #Version:1.0
 
-1、将配置文件的每一行都赋值变量；
-2、使用if判断变量的值
+#脚本名：cisco.sh
+#标识：  改密
+#资源类型: network
+#命令：>en|#$su_pass|#conf t|#username $username password $password |#linie vty 0 4
 
 #==================================================
 #函数1：识别配置文件(structure)
 #==================================================
-
+structure(){ 
+  #count=`sed -n '$=' $config_file`   #统计配置文件行数,加入判断是否有空行
+  #(`awk -F: '{print $2}'`)           #配置文件内容
+  config_file="config_file.txt"
+  #sed -i s/.$// $config_file
+  file_name=`sed -n '1p' $config_file` #要生成的脚本名
+  ltc=`sed -n '2p' $config_file`
+  resource=`sed -n '3p' $config_file`
+  symbol=(`sed -n '4p' $config_file`)
+  count=`echo ${symbol[@]}|awk -F[\ ] '{print NF}'`
+}
 
 #==================================================
 #函数2：windows改密(win_change_password)
@@ -27,7 +39,67 @@
 #==============================================
 #函数4：network改密(net_change_password)
 #==============================================
+net_change_password(){
+cat > $file_name <<EOF
+#!/bin/bash
 
+if [ \$# -eq 6 ];then
+	PROTOCOL=\$1  #远程登录协议 ssh or telnet:超时时间
+	USER=\$2      #管理员用户
+	IP=\$3        #交换机IP
+	PWD=\$4  	  #管理员密码
+	username=\$5  #要更改的用户名
+	password=\$6  #要更改的用户密码
+	
+fi
+
+if [ \$# -eq 7 ];then
+	PROTOCOL=\$1  #远程登录协议 ssh or telnet:超时时间
+	USER=\$2      #管理员用户
+	IP=\$3        #交换机IP
+	PWD=\$4  	  #管理员密码
+	username=\$7  #要更改的用户名
+	password=\$5  #要更改的用户密码
+	SECRET=\$6	  #特权密码
+fi
+PROTOCOL=\`echo \$PROTOCOL | awk -F: '{print \$1}'\` 
+TIMEOUT=\`echo \$PROTOCOL | awk -F: '{print \$2}'\`  #超时时间
+
+case \$PROTOCOL in
+    ssh)
+        CMD="ssh -o StrictHostKeyChecking=no -o CheckHostIP=no \${USER}@\${IP}"
+    ;;
+    telnet)
+        CMD="telnet \${IP}"
+    ;;
+esac
+expect <<EOF
+set timeout \$TIMEOUT
+spawn \$CMD
+expect  "sername:" 
+send "\$USER\r"
+expect "Password:"
+send "\$PWD\r"
+EOF
+
+for (( i = 1; i <=$count; i++ )); do
+	command=`sed -n '5p' config_file.txt | awk -v c="$i" -F"|" '{print $c}'`
+cat >> $file_name <<EOF
+expect "*${symbol[i-1]}"
+send "$command\r"
+EOF
+done
+
+cat >> $file_name <<EEE
+send "exit\r"   
+send "quit\r"
+send "exit\r"   
+send "quit\r" 
+expect eof
+EOF
+EEE
+
+}  
 
 #==============================================
 #函数5：windows账号收集(win_user_collect)
@@ -62,47 +134,52 @@
 #==============================================
 #函数11：配置文件错误提示(config_error)
 #==============================================
+config_error(){
+	echo "配置文件错误"
+	exit 0
+}
 
 Main(){
-  if [[ =="改密" ]]; then
-  	case 类型 in
-  		windows )
+  structure
+  if [[ $ltc=="改密" ]]; then
+  	case $resource in
+  		windows*)
         	win_change_password
   			;;
-  	      linux )
+  	      linux*)
 			linux_change_password
   			;;
-  		network )
+  		network*)
 			net_change_password
   			;;
   		default)
 			config_error
   			;;
   	esac
-  elif [[ =="账号收集" ]]; then
-  	case 类型 in
-  		windows )
+  elif [[ $ltc=="账号收集" ]]; then
+  	case $resource in
+  		windows* )
 			win_user_collect
   			;;
-  	      linux )
+  	      linux* )
 			linux_user_collect
   			;;
-  		network )
+  		network* )
 			net_user_collect
   			;;
   		default)
 			config_error
   			;;
   	esac
-  elif [[ =="添加账号" ]]; then
-  	case 类型 in
-  		windows )
+  elif [[ $ltc=="添加账号" ]]; then
+  	case $resource in
+  		windows* )
 			win_add_user
   			;;
-  	      linux )
+  	      linux* )
 			linux_add_user
   			;;
-  		network )
+  		network*)
 			network_add_user
   			;;
   		default)
